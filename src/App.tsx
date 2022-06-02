0e20066d07e772ab4f9fe38d57b070dc7be20fe0
gabriellopes232 <gabriel.lopes.nasci@hotmail.com>
2022-06-02 09:53:42 -0300

    feature: modal transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
