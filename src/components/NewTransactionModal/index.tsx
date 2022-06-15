14038f9a9f1e61573f34a7ed0ac0e7664f3d0d0d
Gabriellopes232 <xdynatz@gmail.com>
2022-06-15 11:59:28 -0300

    feat: color active color

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -44,6 +44,7 @@ export function NewTransactionModal({
               setType("deposit");
             }}
             isActive={type === "deposit"}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -55,6 +56,7 @@ export function NewTransactionModal({
               setType("withdraw");
             }}
             isActive={type === "withdraw"}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
