0babe3d2b0b34778342fc566ca09851edcd73786
Gabriellopes232 <xdynatz@gmail.com>
2022-06-21 12:39:28 -0300

    refactor: cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import Modal from "react-modal";
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
-import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
-import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from './hooks/useTransactions';
 
-Modal.setAppElement("#root");
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
 
 export function App() {
-  const [isNewTransactionModalOpen, setNewTransactionModalOpen] =
-    useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
 
   function handleOpenNewTransactionModal() {
-    setNewTransactionModalOpen(true);
+    setIsNewTransactionModalOpen(true);
   }
 
   function handleCloseNewTransactionModal() {
-    setNewTransactionModalOpen(false);
+    setIsNewTransactionModalOpen(false);
   }
+
   return (
     <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
+
       <GlobalStyle />
     </TransactionsProvider>
   );
 }
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import { createServer, Model } from "miragejs";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs'
+import { App } from './App';
 
 createServer({
-
   models: {
     transaction: Model,
   },
@@ -14,11 +13,11 @@ createServer({
       transactions: [
         {
           id: 1,
-          title: 'Freelancer de website',
+          title: 'Freelance de website',
           type: 'deposit',
           category: 'Dev',
           amount: 6000,
-          createdAt: new Date()
+          createdAt: new Date('2021-02-12 09:00:00'),
         },
         {
           id: 2,
@@ -26,29 +25,30 @@ createServer({
           type: 'withdraw',
           category: 'Casa',
           amount: 1100,
-          createdAt: new Date('2022-02-12 14:00:00')
+          createdAt: new Date('2021-02-14 11:00:00'),
         }
-      ]
+      ],
     })
   },
 
   routes() {
-    this.namespace = "api";
-    this.get("/transactions", () => {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
       return this.schema.all('transaction')
-    });
+    })
+
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody)
+
       return schema.create('transaction', data)
     })
-  },
-});
+  }
+})
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+  document.getElementById('root')
+); 
